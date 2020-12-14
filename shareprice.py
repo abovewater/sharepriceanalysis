21a2e8093b5c00b1110df5b19bd1b1c39a802bde
James Tew <james.tew88@gmail.com>
2020-12-14 14:54:31 +0800

    update

--- shareprice.py
+++ shareprice.py
@@ -1,15 +1,19 @@
 import pandas_datareader as pdr
 import datetime
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+from csv import reader
 
-#tickers for all the stocks I'm watching
-tickers = ['EOS.AX', 'WES.AX', 'BHP.AX']
+#tickers for all the stocks I'm watching. Listed in a CSV file with all the stocks. 
+with open('shares.csv', 'r') as read_obj:
+    csv_reader = reader(read_obj)
+    tickers = list(csv_reader)
 
-"""
-ask = input("What stock do you want to check? ")
-"""
+#creates a pdf for saving of the plots. To be used in the future. 
+#pp = PdfPages(f'{tickers[i]}.pdf')
 
-#function to return the stock price for the stocks.
+
+#function to return the stock price for the stocks for the date range required. Currently one year.
 def shareprice(ticker):
     stock = pdr.get_data_yahoo(ticker,
                 start=datetime.datetime(2019, 12, 1),
@@ -17,6 +21,8 @@ def shareprice(ticker):
     
     return stock
 
+
+#for loop to plot the SMA for 50,100,150
 for i in range(len(tickers)):
     share = shareprice(tickers[i])
 
@@ -29,6 +35,21 @@ for i in range(len(tickers)):
     share[['Adj Close', '50', '100', '150']].plot()
 
     plt.title(tickers[i])
+    plt.xlabel('Date')
+    plt.ylabel('Share Price')
 
-plt.show()
+    pp = PdfPages(f'{tickers[i]}.pdf')
+
+    plt.savefig(pp, format='pdf')
+    plt.clf()
+    pp.close()
+    
+
+#plt.savefig()
+#plt.show()
+
+
+
+
+
 
